3d519a7b37aaa876ab695f193e672aeb538da47c
Premsingh02 <theaaravkumar02@gmail.com>
2023-11-02 12:16:12 +0530

    javascript inside of curly braces done

--- Day 1/basic-react-app/src/App.jsx
+++ Day 1/basic-react-app/src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css"
 import {Title} from "./Title.jsx"
 
-function Description() {
-  return <h3>I am the Description!</h3>;
-}
-
 function App() {
   return (
-  <div>
-    {/* <h1>This is my app component</h1>
-    <p>inside app component we have :</p> */}
+  <>
+    <Title/> 
     <Title/>
     <Title/>
-  </div>  
+  </>
   );
 }
  
--- Day 1/basic-react-app/src/Title.jsx
+++ Day 1/basic-react-app/src/Title.jsx
@@ -1,8 +1,9 @@
 function Title() {
+    let name = "shradha";
     return (
     <div>
-        <h1>I am the Title!</h1>
-        <h3>I am the Description!</h3>
+       <p>2 * 2 = {2*2}</p>
+       <p>Hi, {name.toUpperCase()}</p>
     </div>
     );
 }
