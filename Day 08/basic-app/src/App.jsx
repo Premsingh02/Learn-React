a239b86c0d624d397e258f11494899409214405b
Premsingh02 <theaaravkumar02@gmail.com>
2023-11-10 12:28:30 +0530

    done with this basic app

--- Day 08/basic-app/src/App.jsx
+++ Day 08/basic-app/src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Tile from './Tile';
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Welcome to react</h1>
-        <span>How r u doing ?</span>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-      </div>
+      <h1 style={{backgroundColor: "red"}}>Welcome to react</h1>
+      <span>How r u doing ?</span>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
     </>
   )
 }
