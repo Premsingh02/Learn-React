5bb5a69be2cb77696c0a2d140af7feb80b58e980
Premsingh02 <theaaravkumar02@gmail.com>
2023-11-11 10:56:45 +0530

    learning about useState Hooks

--- Day 10/game/src/App.jsx
+++ Day 10/game/src/App.jsx
@@ -1,10 +1,42 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [counter, setCounter] = useState(0);
+
+  function clickedIncrement() {  
+    setCount(count+1);
+    console.log("clicked on increment", count);
+  }
+
+  function clickedDecrement() {
+    setCount(count-1);
+    console.log("clicked on decrement", count);
+  }
+
+  function clickedCounterIncrement() {  
+    setCounter(counter+1);
+    console.log("clicked on increment", counter);
+  }
+
+  function clickedCounterDecrement() {
+    setCounter(counter-1);
+    console.log("clicked on decrement", counter);
+  }
 
   return (
     <>
-      Hello
+      <div>
+        value of Count: {count}
+      </div>
+      <button onClick={clickedIncrement}>Increment</button>
+      <button onClick={clickedDecrement}>Decrement</button>
+      <div>
+        value of Counter: {counter}
+      </div>
+      <button onClick={clickedCounterIncrement}>Increment</button>
+      <button onClick={clickedCounterDecrement}>Decrement</button>
     </>
   )
 }
